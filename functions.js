ac852b39ab773d4cb99bf9147740ef8ec3750195
Matt Evans <matthewgcevans@gmail.com>
2022-05-08 19:14:07 +0100

    Add main functions

--- functions.js
+++ functions.js
@@ -1,5 +1,14 @@
 const MAX_GRID_SIZE = 100;
 const DEFAULT_GRID_SIZE = 16;
+const DEFAULT_LINE_COLOUR = '#000000';
+
+let alwaysOn = true;
+let currentlyOn = true;
+let lineColour = DEFAULT_LINE_COLOUR;
+
+function setColor(colour){
+    lineColour = colour;
+}
 
 function resetGrid(){
     const sideSetter = document.querySelector('#sideSetter');
@@ -23,8 +32,10 @@ function setGrid(sideLength){
         let line = createLine(sideLength);
         drawPane.appendChild(line);
     }
+    drawPane.addEventListener('doubleclick', toggleOn);
     const cells = document.querySelectorAll('.cell');
     cells.forEach(cell => cell.addEventListener('mouseover', flipColour));
+    cells.forEach(cell => cell.addEventListener('click', flipColour));
 }
 
 function createLine(lineLength){
@@ -45,11 +56,28 @@ function createLine(lineLength){
 }
 
 function flipColour(e){
-    this.setAttribute('style', 'background-color: black;');
+    if(alwaysOn || currentlyOn){ 
+        this.setAttribute('style', `background-color: ${lineColour};`);
+    }
 }
 
+function toggleOn(e){
+    alwaysOn = !alwaysOn;
+    currentlyOn = alwaysOn;
+    if(alwaysOn){
+        alwaysOnToggle.textContent = "Switch to draw on click";
+    } else {
+        alwaysOnToggle.textContent = "Switch to always on";
+    }
+}
 
-setGrid(DEFAULT_GRID_SIZE);
+function highlightButton(e){
+    if(!this.classList.contains('selected')){
+        this.classList.add('selected');
+    } else {
+        this.classList.remove('selected');
+    }
+}
 
 const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', resetGrid);
@@ -58,3 +86,27 @@ const sideSetter = document.querySelector('#sideSetter');
 sideSetter.oninput = function() {
     setGrid(this.value);
 }
+
+const cPicker = document.querySelector('#cPicker');
+cPicker.oninput = function() {
+    setColor(this.value);
+}
+
+function drawActive() { currentlyOn = true; }
+
+function drawDisable() { currentlyOn = false; }
+
+const alwaysOnToggle = document.querySelector('#alwaysOn');
+alwaysOnToggle.addEventListener('click', toggleOn);
+
+const workArea = document.querySelector('#drawPane');
+workArea.addEventListener('dblclick', toggleOn);
+
+const w = document.querySelector('body');
+w.addEventListener('mousedown', drawActive);
+w.addEventListener('mouseup', drawDisable);
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', highlightButton));
+
+setGrid(DEFAULT_GRID_SIZE);
