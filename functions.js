e75c4dd9d5f6f75b66ea7ce7249192af7eedd5bf
Matt Evans <matthewgcevans@gmail.com>
2022-05-08 14:44:41 +0100

    Basic version created

--- functions.js
+++ functions.js
@@ -1,5 +1,24 @@
+const MAX_GRID_SIZE = 100;
+const DEFAULT_GRID_SIZE = 16;
+
+function resetGrid(){
+    const sideSetter = document.querySelector('#sideSetter');
+    sideSetter.value = DEFAULT_GRID_SIZE;
+    setGrid(DEFAULT_GRID_SIZE);
+}
+
+function changeGridSize(e){
+    console.log(e.value);
+}
+
 function setGrid(sideLength){
+    if(sideLength > MAX_GRID_SIZE){
+        sideLength = MAX_GRID_SIZE;
+    }
+    const sizePanel = document.querySelector('#gridSize');
+    sizePanel.textContent = `Grid size: ${sideLength} x ${sideLength}`;
     const drawPane = document.querySelector('#drawPane');
+    drawPane.replaceChildren();
     for(let i = 0; i < sideLength; i++){
         let line = createLine(sideLength);
         drawPane.appendChild(line);
@@ -30,4 +49,12 @@ function flipColour(e){
 }
 
 
-setGrid(2);
+setGrid(DEFAULT_GRID_SIZE);
+
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', resetGrid);
+
+const sideSetter = document.querySelector('#sideSetter');
+sideSetter.oninput = function() {
+    setGrid(this.value);
+}
