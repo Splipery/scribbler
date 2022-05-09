020200277e261947a7cec1d310b3a621145be99c
Matt Evans <matthewgcevans@gmail.com>
2022-05-09 22:00:53 +0100

    Add shading function

--- functions.js
+++ functions.js
@@ -1,11 +1,12 @@
 const MAX_GRID_SIZE = 100;
 const DEFAULT_GRID_SIZE = 16;
-const DEFAULT_LINE_COLOUR = '#000000';
+const DEFAULT_LINE_COLOUR = '#000';
 
 let alwaysOn = true;
 let currentlyOn = true;
 let randomColours = false;
 let lineColour = DEFAULT_LINE_COLOUR;
+let shading = false;
 
 function setColor(colour){
     lineColour = colour;
@@ -17,14 +18,11 @@ function resetGrid(){
     setGrid(DEFAULT_GRID_SIZE);
 }
 
-function changeGridSize(e){
-    console.log(e.value);
-}
-
 function setGrid(sideLength){
     if(sideLength > MAX_GRID_SIZE){
         sideLength = MAX_GRID_SIZE;
     }
+    lineColour = cPicker.value;
     const sizePanel = document.querySelector('#gridSize');
     sizePanel.textContent = `Grid size: ${sideLength} x ${sideLength}`;
     const drawPane = document.querySelector('#drawPane');
@@ -36,7 +34,6 @@ function setGrid(sideLength){
     drawPane.addEventListener('doubleclick', toggleOn);
     const cells = document.querySelectorAll('.cell');
     cells.forEach(cell => cell.addEventListener('mouseover', flipColour));
-    cells.forEach(cell => cell.addEventListener('click', flipColour));
 }
 
 function createLine(lineLength){
@@ -51,29 +48,32 @@ function createLine(lineLength){
         nextCell.setAttribute('style', `width: ${paneWidth/lineLength}; min-height: ${paneWidth/lineLength}`);
         lineDiv.appendChild(nextCell);
     }
-    console.log(lineDiv);
     return lineDiv;
 }
 
 function flipColour(e){
-    if(alwaysOn || currentlyOn){ 
+    if(alwaysOn || currentlyOn){
+        let currentColor = window.getComputedStyle(this).getPropertyValue('background-color');
+        let currentOpacity = (window.getComputedStyle(this).getPropertyValue('opacity')) * 1;
         if(randomColours) { 
             lineColour = '#' + Math.floor(Math.random()*16777215).toString(16); 
-            console.log(lineColour);
+            currentOpacity = 1;
+        } else {
+            if(currentOpacity < 1 && shading){
+                currentOpacity += 0.1;
+            } else if (shading){
+                currentOpacity = 0.1;
+            } else {
+                currentOpacity = 1;
+            }
         }
-        this.setAttribute('style', `background-color: ${lineColour};`);
-        
+        this.setAttribute('style', `background-color: ${lineColour}; opacity: ${currentOpacity};`);
     }
 }
 
 function toggleOn(e){
     alwaysOn = !alwaysOn;
     currentlyOn = alwaysOn;
-    if(!alwaysOnToggle.classList.contains('selected')){
-        alwaysOnToggle.classList.add('selected');
-    } else {
-        alwaysOnToggle.classList.remove('selected');
-    }
 }
 
 function highlightButton(e) {
@@ -104,13 +104,19 @@ function drawDisable() { currentlyOn = false; }
 function toggleRandomise() { 
     randomColours = !randomColours;
     setColor(cPicker.value);
+    if(shading){
+        shading = false;
+        document.querySelector('#shading').classList.remove('selected');
+    }
+    console.log(`RANDOMISER: ${shading}:${randomColours}`);
 }
+
 
 const alwaysOnToggle = document.querySelector('#alwaysOn');
 alwaysOnToggle.addEventListener('click', toggleOn);
 
 const workArea = document.querySelector('#drawPane');
-workArea.addEventListener('dblclick', toggleOn);
+workArea.addEventListener('dblclick', function() {alwaysOnToggle.click();});
 
 const w = document.querySelector('body');
 w.addEventListener('mousedown', drawActive);
@@ -120,7 +126,18 @@ const randomButton = document.querySelector('#randomColour');
 randomButton.addEventListener('click', toggleRandomise);
 randomButton.addEventListener('click', highlightButton);
 
-const toggles = document.querySelectorAll('#toggles');
+const toggles = document.querySelectorAll('.toggles');
 toggles.forEach(toggle => toggle.addEventListener('click', highlightButton));
+
+const shadingToggle = document.querySelector('#shading');
+shadingToggle.addEventListener('click', 
+    function(){ 
+        shading = !shading;
+        if(randomColours){
+            randomColours = false;
+            randomButton.classList.remove('selected');
+        }
+        setColor(cPicker.value);
+    });
 
 setGrid(DEFAULT_GRID_SIZE);
